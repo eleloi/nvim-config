753498d8f3f991bd74dcf5a4e43572c1434895af
Eloi Vallverdú <eleloi@gmail.com>
2024-08-08 21:57:01 +0200

    add scratchpad to zen mode

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,4 +1,18 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
+	version = "*",
 	keys = { { "<leader>zz", "<cmd>NoNeckPain<cr>", desc = "Zen mode" } },
+	config = function()
+		require("no-neck-pain").setup({
+			buffers = {
+				scratchPad = {
+					enabled = true,
+					location = "~/Documents/scratchpad",
+				},
+				bo = {
+					filetype = "md",
+				},
+			},
+		})
+	end,
 }
