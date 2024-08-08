e4151af35870eef9b07386a9646f7d5b96b1d1d8
Eloi Vallverdú <eleloi@gmail.com>
2024-08-08 21:56:27 +0200

    refactor huez key

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,15 @@
 return {
 	{
 		"vague2k/huez.nvim",
-		dependencies = { "sainnhe/gruvbox-material" },
+		dependencies = {
+			"sainnhe/gruvbox-material",
+			"0xstepit/flow.nvim",
+		},
 		import = "huez-manager.import",
 		lazy = false,
+		priority = 1000,
 		keys = {
-			{ "<leader>fch", "<CMD>Huez<CR>", desc = "Huez Manager" },
+			{ "<leader>fcc", "<CMD>Huez<CR>", desc = "Huez Manager" },
 			{ "<leader>fcl", "<CMD>HuezLive<CR>", desc = "Huez Live" },
 			{ "<leader>fcf", "<CMD>HuezFavorites<CR>", desc = "Huez Favorites" },
 		},
