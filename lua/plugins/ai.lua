8bdec11a6c5c0e1901f3f90b05bef0101128ad48
Eloi Vallverdú <eleloi@gmail.com>
2024-09-06 20:22:09 +0200

    refactor ai plugins location

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,5 +1,33 @@
 return {
 	{
+		"Bryley/neoai.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = {
+			"NeoAI",
+			"NeoAIOpen",
+			"NeoAIClose",
+			"NeoAIToggle",
+			"NeoAIContext",
+			"NeoAIContextOpen",
+			"NeoAIContextClose",
+			"NeoAIInject",
+			"NeoAIInjectCode",
+			"NeoAIInjectContext",
+			"NeoAIInjectContextCode",
+		},
+		keys = {
+			{ "<leader>as", desc = "summarize text" },
+			{ "<leader>ag", desc = "generate git message" },
+		},
+		config = function()
+			require("neoai").setup({
+				-- Options go here
+			})
+		end,
+	},
+	{
 		"Exafunction/codeium.vim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
