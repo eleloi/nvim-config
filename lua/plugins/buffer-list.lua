e27d99cbb78c4561eeef01b9018dea53c2480bbf
Eloi Vallverdú <eleloi@gmail.com>
2024-10-24 13:43:48 +0200

    add buffer list plugin

---
+++ lua/plugins/buffer-list.lua
@@ -0,0 +1,12 @@
+return {
+	"j-morano/buffer_manager.nvim",
+	keys = {
+		{
+			"<leader>.",
+			function()
+				require("buffer_manager.ui").toggle_quick_menu()
+			end,
+			desc = "Buffer List",
+		},
+	},
+}
