4bf9e576e23739af08e59c0bf8a0e3698a3b1a30
Eloi Vallverdú <eleloi@gmail.com>
2024-10-31 11:27:54 +0100

    style: telescope references no line information

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -55,7 +55,10 @@ local keys = {
 	{
 		"gr",
 		function()
-			require("telescope.builtin").lsp_references({ jump_type = "never" })
+			require("telescope.builtin").lsp_references({
+				jump_type = "never",
+				show_line = false,
+			})
 		end,
 		desc = "Lsp References",
 		mode = "n",
@@ -185,7 +188,7 @@ return {
 		"nvim-telescope/telescope.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 		},
 		"xiyaowong/telescope-emoji.nvim",
 		"piersolenski/telescope-import.nvim",
