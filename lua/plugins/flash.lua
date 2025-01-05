cc7772257c38f3103075f0032988672b79f72d14
Eloi Vallverdú <eleloi@gmail.com>
2025-01-05 14:05:01 +0100

    remove remote flash keys

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,7 +4,6 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
 	keys = {
 		{
 			"s",
@@ -22,29 +21,29 @@ return {
 			end,
 			desc = "Flash Treesitter",
 		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
-		},
-		{
-			"<c-s>",
-			mode = { "c" },
-			function()
-				require("flash").toggle()
-			end,
-			desc = "Toggle Flash Search",
-		},
+		-- {
+		-- 	"r",
+		-- 	mode = "o",
+		-- 	function()
+		-- 		require("flash").remote()
+		-- 	end,
+		-- 	desc = "Remote Flash",
+		-- },
+		-- {
+		-- 	"R",
+		-- 	mode = { "o", "x" },
+		-- 	function()
+		-- 		require("flash").treesitter_search()
+		-- 	end,
+		-- 	desc = "Treesitter Search",
+		-- },
+		-- {
+		-- 	"<c-s>",
+		-- 	mode = { "c" },
+		-- 	function()
+		-- 		require("flash").toggle()
+		-- 	end,
+		-- 	desc = "Toggle Flash Search",
+		-- },
 	},
 }
