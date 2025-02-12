179867050cffdfcd4b87d2ef26a64762e2b48d68
Eloi Vallverdú <eleloi@gmail.com>
2025-02-12 20:40:42 +0100

    add: tailwind colors and other tools

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,19 +1,32 @@
 -- View colors in certain files
 return {
-  "norcalli/nvim-colorizer.lua",
-  event = "VeryLazy",
-  config = function()
-    require("colorizer").setup({
-      "css",
-      "javascript",
-      "typesciprt",
-      "javascriptreact",
-      "typescriptreact",
-      "html",
-      "lua",
-      "python",
-      "json",
-      "conf",
-    })
-  end,
+  {
+    "norcalli/nvim-colorizer.lua",
+    event = "VeryLazy",
+    config = function()
+      require("colorizer").setup({
+        "css",
+        "javascript",
+        "typesciprt",
+        "javascriptreact",
+        "typescriptreact",
+        "html",
+        "lua",
+        "python",
+        "json",
+        "conf",
+      })
+    end,
+  },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {},
+  },
 }
