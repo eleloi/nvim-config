28a4a156c4d0f1fca5c3a3ed7c35ae7ebe208156
Eloi Vallverdú <eleloi@gmail.com>
2025-02-15 11:37:57 +0100

    format files

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,114 +1,114 @@
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        "LazyVim",
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod",                     lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-      { "moyiz/blink-emoji.nvim" },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
-    end,
-  },
-  {
-    "saghen/blink.cmp",
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				"lazy.nvim",
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+			{ "moyiz/blink-emoji.nvim" },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+	{
+		"saghen/blink.cmp",
 
-    dependencies = {
-      "mikavilpas/blink-ripgrep.nvim",
-      "rafamadriz/friendly-snippets",
-    },
+		dependencies = {
+			"mikavilpas/blink-ripgrep.nvim",
+			"rafamadriz/friendly-snippets",
+		},
 
-    version = "*",
+		version = "*",
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = {
-        preset = "default",
-        ["<C-y>"] = {},
-        ["<C-l>"] = { "select_and_accept" },
-        ["<C-n>"] = { "show", "select_next", "fallback" },
-      },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = {
+				preset = "default",
+				["<C-y>"] = {},
+				["<C-l>"] = { "select_and_accept" },
+				["<C-n>"] = { "show", "select_next", "fallback" },
+			},
 
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
-      },
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
 
-      signature = { enabled = true },
-      sources = {
-        default = {
-          "lsp",
-          "lazydev",
-          "path",
-          "snippets",
-          "dadbod",
-          "buffer",
-          "ripgrep",
-          "emoji",
-        },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
-          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "Ripgrep",
-          },
-          emoji = {
-            module = "blink-emoji",
-            name = "Emoji",
-            score_offset = 15,
-            opts = { insert = true },
-          },
-          snippets = {
-            name = "snippets",
-          },
-        },
-      },
+			signature = { enabled = true },
+			sources = {
+				default = {
+					"lsp",
+					"lazydev",
+					"path",
+					"snippets",
+					"dadbod",
+					"buffer",
+					"ripgrep",
+					"emoji",
+				},
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
+					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+					ripgrep = {
+						module = "blink-ripgrep",
+						name = "Ripgrep",
+					},
+					emoji = {
+						module = "blink-emoji",
+						name = "Emoji",
+						score_offset = 15,
+						opts = { insert = true },
+					},
+					snippets = {
+						name = "snippets",
+					},
+				},
+			},
 
-      completion = {
-        trigger = {
-          show_on_keyword = true,
-        },
-        menu = {
-          draw = {
-            columns = {
-              { "kind_icon",   "label", "label_description", gap = 1 },
-              { "source_name", "kind",  gap = 1 },
-            },
-            treesitter = { "lsp" },
-          },
-        },
-        list = {
-          selection = { auto_insert = true, preselect = true },
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 100,
-        },
-      },
-    },
-    opts_extend = { "sources.default" },
-  },
+			completion = {
+				trigger = {
+					show_on_keyword = true,
+				},
+				menu = {
+					draw = {
+						columns = {
+							{ "kind_icon", "label", "label_description", gap = 1 },
+							{ "source_name", "kind", gap = 1 },
+						},
+						treesitter = { "lsp" },
+					},
+				},
+				list = {
+					selection = { auto_insert = true, preselect = true },
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 100,
+				},
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
 }
