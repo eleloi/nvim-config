c73c58fd16114556c71a55d8841377e85c9900da
Eloi Vallverdú <eleloi@gmail.com>
2025-03-27 16:58:33 +0100

    connect vimwiki with taskwarrior

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,28 +1,34 @@
 return {
-    "vimwiki/vimwiki",
-    -- event = "BufEnter *.md",
-    event = "VeryLazy",
-    keys = { {
-        "<leader>ww",
-        "<cmd>VimwikiIndex<cr>",
-        desc = "Vimwiki Index"
-    }, {
-        "<leader>wt",
-        "<cmd>VimwikiMakeDiaryNote<cr>",
-        desc = "Vimwiki Make Diary Note"
+    {
+        "vimwiki/vimwiki",
+        -- event = "BufEnter *.md",
+        event = "VeryLazy",
+        keys = { {
+            "<leader>ww",
+            "<cmd>VimwikiIndex<cr>",
+            desc = "Vimwiki Index"
+        }, {
+            "<leader>wd",
+            "<cmd>VimwikiMakeDiaryNote<cr>",
+            desc = "Vimwiki Make Diary Note"
 
-    } },
-    -- The configuration for the plugin
-    init = function()
-        vim.g.vimwiki_global_ext = 0
-        vim.g.vimwiki_list = {
-            {
-                path = "~/Nextcloud/Notes/",
-                syntax = "markdown",
-                ext = "md",
-            },
-        }
-        -- vim.g.vimwiki_filetypes = { "markdown" }
-        -- vim.treesitter.language.register("markdown", { "vimwiki" })
-    end,
+        } },
+        -- The configuration for the plugin
+        init = function()
+            vim.g.vimwiki_global_ext = 0
+            vim.g.vimwiki_list = {
+                {
+                    path = "~/Nextcloud/Notes/",
+                    syntax = "markdown",
+                    ext = "md",
+                },
+            }
+            -- vim.g.vimwiki_filetypes = { "markdown" }
+            -- vim.treesitter.language.register("markdown", { "vimwiki" })
+        end,
+    },
+    {
+        "tools-life/taskwiki",
+        event = "VeryLazy",
+    }
 }
