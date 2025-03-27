bea84ccc6fd9bb9b918e149b991da54552fed41c
Eloi Vallverdú <eleloi@gmail.com>
2025-03-27 16:58:18 +0100

    add markview preset format

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -8,11 +8,13 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
+        local preset = require("markview.presets")
         vim.treesitter.language.register("markdown", { "vimwiki" })
         require("markview").setup({
             preview = {
                 filetypes = { "markdown", "vimwiki" },
-            }
+            },
+            markdown = { headings = preset.headings.glow }
         })
         vim.cmd("Markview Disable")
     end,
