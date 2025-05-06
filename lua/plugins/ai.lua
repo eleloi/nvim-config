87df158424bf15314f44c913142cbcf4d264d57d
Eloi Vallverdú <eleloi@gmail.com>
2025-05-06 11:51:49 +0200

    disable python tools for avante claude stuff

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -62,7 +62,8 @@ return {
             provider = "claude",
             claude = {
                 model = "claude-3-7-sonnet-20250219",
-                disable_tools = true, -- Disable tools for now (it's enabled by default) as it's causing rate-limit problems with Claude, see more here: https://github.com/yetone/avante.nvim/issues/1384
+                -- disable_tools = true, -- Disable tools for now (it's enabled by default) as it's causing rate-limit problems with Claude, see more here: https://github.com/yetone/avante.nvim/issues/1384
+                disable_tools = { "python" }
             },
             behaviour = {
                 auto_suggestions = false,
