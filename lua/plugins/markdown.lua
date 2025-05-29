8ec55aa07a6ac607432871b3b9961bde9f1f761c
Eloi Vallverdú <eleloi@gmail.com>
2025-05-29 13:24:34 +0200

    feat(markdown): add codecompanion filetype support to render-markdown
    Added "codecompanion" to the list of supported filetypes for the render-markdown.nvim plugin, enabling improved markdown rendering for this filetype.

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,13 +1,13 @@
 -- Improve markdown view
 
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    ft = { "markdown", "Avante", "vimwiki" },
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "Avante", "vimwiki", "codecompanion" },
     config = function()
         vim.g.render_markdown_enable = true
         vim.treesitter.language.register("markdown", "vimwiki")
         require("render-markdown").setup({
-            file_types = { "markdown", "Avante", "vimwiki" },
+            file_types = { "markdown", "Avante", "vimwiki", "codecompanion" },
         })
-    end
+    end,
 }
