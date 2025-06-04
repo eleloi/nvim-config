d2acfe89e23b8c510eef0b4fbc56992e3e868e9b
Eloi Vallverdú <eleloi@gmail.com>
2025-06-04 12:40:02 +0200

    feat(none-ls): enable plugin with mason integration
    - Enable none-ls plugin (previously disabled)
    - Add mason-null-ls and mason dependencies for automatic tool management
    - Replace manual source configuration with mason-null-ls setup
    - Configure automatic installation of formatting and code action tools
    - Simplify null-ls configuration by removing explicit sources
    - Update indentation from spaces to tabs

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,37 +1,46 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ async = false })
-            end,
-        })
-    end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end
 end
 
 return {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    config = function()
-        local null_ls = require("null-ls")
-        require("null-ls").setup({
-            on_attach = on_attach,
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettierd.with({
-                    extra_filetypes = { "svelte", "svg", "astro" },
-                }),
-                null_ls.builtins.code_actions.gomodifytags,
-                null_ls.builtins.code_actions.impl,
-                null_ls.builtins.formatting.goimports,
-                null_ls.builtins.formatting.gofumpt,
-                null_ls.builtins.formatting.pg_format,
-                null_ls.builtins.formatting.nixfmt,
-            },
-        })
-    end,
+	"nvimtools/none-ls.nvim",
+	event = "VeryLazy",
+	enabled = true,
+	dependencies = {
+		"jay-babu/mason-null-ls.nvim",
+		"williamboman/mason.nvim",
+	},
+	config = function()
+		-- Setup mason-null-ls first
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"stylua",
+				"biome",
+				"gomodifytags",
+				"impl",
+				"goimports",
+				"gofumpt",
+				"pg_format",
+				"nixfmt",
+			},
+			automatic_installation = false,
+			handlers = {},
+		})
+
+		-- Then setup none-ls
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			on_attach = on_attach,
+		})
+	end,
 }
