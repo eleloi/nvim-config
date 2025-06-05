69a2ec50bfc07c14f50035cdb48ad7e864374b4e
Eloi Vallverdú <eleloi@gmail.com>
2025-06-05 16:47:23 +0200

    feat(vimwiki): add backlinks keybinding and link.vim plugin
    - Add <leader>wb keybinding for VimwikiBacklinks command
    - Add qadzek/link.vim plugin with <leader>wl keybinding for LinkConvertAll
    - Standardize code formatting (tabs, uppercase command syntax)

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,28 +1,48 @@
 return {
-    {
-        "vimwiki/vimwiki",
-        -- event = "BufEnter *.md",
-        event = "VeryLazy",
-        keys = { {
-            "<leader>ww",
-            "<cmd>VimwikiIndex<cr>",
-            desc = "Vimwiki Index"
-        }, {
-            "<leader>wd",
-            "<cmd>VimwikiMakeDiaryNote<cr>",
-            desc = "Vimwiki Make Diary Note"
-
-        } },
-        -- The configuration for the plugin
-        init = function()
-            vim.g.vimwiki_global_ext = 0
-            vim.g.vimwiki_list = {
-                {
-                    path = "~/Documents/vimwiki/",
-                    syntax = "markdown",
-                    ext = "md",
-                },
-            }
-        end,
-    },
+	{
+		"vimwiki/vimwiki",
+		-- event = "BufEnter *.md",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>ww",
+				"<CMD>VimwikiIndex<CR>",
+				desc = "Vimwiki Index",
+			},
+			{
+				"<leader>wd",
+				"<CMD>VimwikiMakeDiaryNote<CR>",
+				desc = "Vimwiki Make Diary Note",
+			},
+			{
+				"<leader>wb",
+				"<CMD>VimwikiBacklinks<CR>",
+				desc = "BackLinks",
+			},
+		},
+		-- The configuration for the plugin
+		init = function()
+			vim.g.vimwiki_global_ext = 0
+			vim.g.vimwiki_list = {
+				{
+					path = "~/Documents/vimwiki/",
+					syntax = "markdown",
+					ext = "md",
+				},
+			}
+		end,
+	},
+	{
+		"qadzek/link.vim",
+		init = function()
+			vim.g.link_use_default_mappings = false
+		end,
+		keys = {
+			{
+				"<leader>wl",
+				"<CMD>LinkConvertAll<CR>",
+				desc = "Add plugin config or keymap for Vimwiki links",
+			},
+		},
+	},
 }
