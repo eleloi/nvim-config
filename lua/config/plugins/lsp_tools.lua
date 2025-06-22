aba2882a58b8cfcdfa02fe93a924b988487b8db7
Eloi Vallverdú <eleloi@gmail.com>
2025-06-23 01:39:05 +0200

    feat(lsp): switch to basedpyright and add floating diagnostics
    - Replace pyright with basedpyright LSP server
    - Add float parameter to diagnostic jump navigation
    - Reformat code with consistent spacing

--- lua/config/plugins/lsp_tools.lua
+++ lua/config/plugins/lsp_tools.lua
@@ -1,128 +1,130 @@
 return {
-	mason = {
-		lua_ls = {
-			settings = { Lua = { telemetry = { enable = false } } },
-			on_init = function(client)
-				if client.workspace_folders then
-					local path = client.workspace_folders[1].name
-					if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
-						return
-					end
-				end
+    mason = {
+        lua_ls = {
+            settings = { Lua = { telemetry = { enable = false } } },
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+                        return
+                    end
+                end
 
-				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-					runtime = {
-						version = "LuaJIT",
-					},
-					workspace = {
-						checkThirdParty = true,
-						library = {
-							vim.env.VIMRUNTIME,
-							"~/.local/share/nvim/lazy",
-							"/home/eleloi/.config/nvim/lazy",
-							"${3rd}/luv/library",
-						},
-					},
-				})
-			end,
-		},
-		rust_analyzer = {},
-		eslint = {},
-		pyright = {},
-		ruff = {},
-		gopls = {},
-		biome = {},
-		golangci_lint_ls = {},
-		tinymist = {
-			filetypes = { "typst", "typ" },
-			settings = {
-				formatterMode = "typstyle",
-				exportPdf = "onType",
-				semanticTokens = "disable",
-			},
-		},
-		astro = {
-			on_init = function()
-				vim.g.astro_typescript = "enable"
-			end,
-		},
-		bashls = { filetypes = { "bash", "sh", "zsh" } },
-		ts_ls = {},
-		svelte = {},
-		tailwindcss = {},
-		docker_compose_language_service = {},
-		dockerls = {},
-		yamlls = {
-			settings = { redhat = { telemetry = { enable = false } } },
-		},
-		taplo = {},
-		jsonls = {
-			settings = {
-				json = {
-					-- Schemas https://www.schemastore.org
-					schemas = {
-						{
-							fileMatch = { "package.json" },
-							url = "https://json.schemastore.org/package.json",
-						},
-						{
-							fileMatch = { "tsconfig*.json" },
-							url = "https://json.schemastore.org/tsconfig.json",
-						},
-						{
-							fileMatch = {
-								".prettierrc",
-								".prettierrc.json",
-								"prettier.config.json",
-							},
-							url = "https://json.schemastore.org/prettierrc.json",
-						},
-						{
-							fileMatch = { ".eslintrc", ".eslintrc.json" },
-							url = "https://json.schemastore.org/eslintrc.json",
-						},
-						{
-							fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
-							url = "https://json.schemastore.org/babelrc.json",
-						},
-						{
-							fileMatch = { "lerna.json" },
-							url = "https://json.schemastore.org/lerna.json",
-						},
-						{
-							fileMatch = { "now.json", "vercel.json" },
-							url = "https://json.schemastore.org/now.json",
-						},
-						{
-							fileMatch = {
-								".stylelintrc",
-								".stylelintrc.json",
-								"stylelint.config.json",
-							},
-							url = "http://json.schemastore.org/stylelintrc.json",
-						},
-					},
-				},
-			},
-		},
-	},
-	noMason = {
-		nushell = {},
-		nixd = {
-			cmd = { "nixd" },
-			settings = {
-				nixpkgs = {
-					expr = "import <nixpkgs> { }",
-				},
-				options = {
-					nixos = {
-						expr = '(builtins.getFlake "/home/eleloi/nixos-config/flake.nix").nixosConfigurations."bob".options',
-					},
-					home_manager = {
-						expr = '(builtins.getFlake "/home/eleloi/nixos-config/flake.nix").homeConfigurations."eleloi".options',
-					},
-				},
-			},
-		},
-	},
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    workspace = {
+                        checkThirdParty = true,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            "~/.local/share/nvim/lazy",
+                            "/home/eleloi/.config/nvim/lazy",
+                            "${3rd}/luv/library",
+                        },
+                    },
+                })
+            end,
+        },
+        rust_analyzer = {},
+        eslint = {},
+        basedpyright = {},
+        ruff = {},
+        gopls = {},
+        biome = {},
+        golangci_lint_ls = {},
+        tinymist = {
+            filetypes = { "typst", "typ" },
+            settings = {
+                formatterMode = "typstyle",
+                exportPdf = "onType",
+                semanticTokens = "disable",
+            },
+        },
+        astro = {
+            on_init = function()
+                vim.g.astro_typescript = "enable"
+            end,
+        },
+        bashls = { filetypes = { "bash", "sh", "zsh" } },
+        ts_ls = {},
+        svelte = {},
+        tailwindcss = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        yamlls = {
+            settings = { redhat = { telemetry = { enable = false } } },
+        },
+        taplo = {},
+        jsonls = {
+            settings = {
+                json = {
+                    -- Schemas https://www.schemastore.org
+                    schemas = {
+                        {
+                            fileMatch = { "package.json" },
+                            url = "https://json.schemastore.org/package.json",
+                        },
+                        {
+                            fileMatch = { "tsconfig*.json" },
+                            url = "https://json.schemastore.org/tsconfig.json",
+                        },
+                        {
+                            fileMatch = {
+                                ".prettierrc",
+                                ".prettierrc.json",
+                                "prettier.config.json",
+                            },
+                            url = "https://json.schemastore.org/prettierrc.json",
+                        },
+                        {
+                            fileMatch = { ".eslintrc", ".eslintrc.json" },
+                            url = "https://json.schemastore.org/eslintrc.json",
+                        },
+                        {
+                            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+                            url = "https://json.schemastore.org/babelrc.json",
+                        },
+                        {
+                            fileMatch = { "lerna.json" },
+                            url = "https://json.schemastore.org/lerna.json",
+                        },
+                        {
+                            fileMatch = { "now.json", "vercel.json" },
+                            url = "https://json.schemastore.org/now.json",
+                        },
+                        {
+                            fileMatch = {
+                                ".stylelintrc",
+                                ".stylelintrc.json",
+                                "stylelint.config.json",
+                            },
+                            url = "http://json.schemastore.org/stylelintrc.json",
+                        },
+                    },
+                },
+            },
+        },
+    },
+    noMason = {
+        nushell = {},
+        nixd = {
+            cmd = { "nixd" },
+            settings = {
+                nixpkgs = {
+                    expr = "import <nixpkgs> { }",
+                },
+                options = {
+                    nixos = {
+                        expr =
+                        '(builtins.getFlake "/home/eleloi/nixos-config/flake.nix").nixosConfigurations."bob".options',
+                    },
+                    home_manager = {
+                        expr =
+                        '(builtins.getFlake "/home/eleloi/nixos-config/flake.nix").homeConfigurations."eleloi".options',
+                    },
+                },
+            },
+        },
+    },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,78 +1,78 @@
 local lsp_tools = require("config.plugins.lsp_tools")
 
 local on_attach = function(_, bufnr)
-	local opts = { buffer = bufnr }
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-	vim.keymap.set("n", "gD", function()
-		vim.api.nvim_command("vsplit")
-		vim.lsp.buf.definition()
-	end, opts)
-	vim.keymap.set("n", "<leader>cD", vim.lsp.buf.type_definition, opts)
-	vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-	vim.cmd.autocmd("BufWritePre", "<buffer>", "lua vim.lsp.buf.format()")
+    local opts = { buffer = bufnr }
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gD", function()
+        vim.api.nvim_command("vsplit")
+        vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "<leader>cD", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.cmd.autocmd("BufWritePre", "<buffer>", "lua vim.lsp.buf.format()")
 end
 
 return {
-	{
-		"williamboman/mason.nvim",
-		config = true,
-		event = "VeryLazy",
-	},
-	{
-		"neovim/nvim-lspconfig",
-		event = "VeryLazy",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"davidosomething/format-ts-errors.nvim",
-			"saghen/blink.cmp",
-		},
-		config = function()
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = true,
+        event = "VeryLazy",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = "VeryLazy",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "davidosomething/format-ts-errors.nvim",
+            "saghen/blink.cmp",
+        },
+        config = function()
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			local all_servers = vim.tbl_extend("force", lsp_tools.mason, lsp_tools.noMason)
+            local all_servers = vim.tbl_extend("force", lsp_tools.mason, lsp_tools.noMason)
 
-			vim.filetype.add({
-				pattern = {
-					["compose.*%.ya?ml"] = "yaml.docker-compose",
-					["docker%-compose.*%.ya?ml"] = "yaml.docker-compose",
-				},
-			})
-			vim.keymap.set("n", "[d", function()
-				vim.diagnostic.jump({ count = -1 })
-			end)
-			vim.keymap.set("n", "]d", function()
-				vim.diagnostic.jump({ count = 1 })
-			end)
+            vim.filetype.add({
+                pattern = {
+                    ["compose.*%.ya?ml"] = "yaml.docker-compose",
+                    ["docker%-compose.*%.ya?ml"] = "yaml.docker-compose",
+                },
+            })
+            vim.keymap.set("n", "[d", function()
+                vim.diagnostic.jump({ count = -1, float = true })
+            end)
+            vim.keymap.set("n", "]d", function()
+                vim.diagnostic.jump({ count = 1, float = true })
+            end)
 
-			local base_params = {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			}
-			for server_name, params in pairs(all_servers) do
-				vim.lsp.enable(server_name)
-				local server_params = vim.tbl_extend("force", base_params, params)
-				vim.lsp.config[server_name] = server_params
-			end
-		end,
-	},
+            local base_params = {
+                on_attach = on_attach,
+                capabilities = capabilities,
+            }
+            for server_name, params in pairs(all_servers) do
+                vim.lsp.enable(server_name)
+                local server_params = vim.tbl_extend("force", base_params, params)
+                vim.lsp.config[server_name] = server_params
+            end
+        end,
+    },
 
-	-- use mason-lspconfig to install lsp tools automatically and setup lspconfig
-	{
-		"mason-org/mason-lspconfig.nvim",
-		enabled = true,
-		dependencies = { { "mason-org/mason.nvim", opts = {} } },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = vim.tbl_keys(lsp_tools.mason),
-				automatic_enable = false,
-			})
-		end,
-	},
-	{
-		"dmmulroy/tsc.nvim",
-		ft = { "typescript", "typescriptreact" },
-		config = true,
-	},
+    -- use mason-lspconfig to install lsp tools automatically and setup lspconfig
+    {
+        "mason-org/mason-lspconfig.nvim",
+        enabled = true,
+        dependencies = { { "mason-org/mason.nvim", opts = {} } },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = vim.tbl_keys(lsp_tools.mason),
+                automatic_enable = false,
+            })
+        end,
+    },
+    {
+        "dmmulroy/tsc.nvim",
+        ft = { "typescript", "typescriptreact" },
+        config = true,
+    },
 }
