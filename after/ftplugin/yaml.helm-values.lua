c7c96198fc45a5545869e4da759270549bbf3c91
Eloi Vallverdú <eleloi@gmail.com>
2025-10-05 23:52:39 +0200

    feat(helm): Add helm-ls plugin and ftplugin settings
    - Added the `qvalentin/helm-ls.nvim` plugin for Helm chart support.
    - Created a new ftplugin file for `yaml.helm-values` to set specific
      indentation and enable treesitter-context for these files.

---
+++ after/ftplugin/yaml.helm-values.lua
@@ -0,0 +1,30 @@
+local set = vim.bo
+
+set.tabstop = 2
+set.softtabstop = 2
+set.expandtab = true
+set.shiftwidth = 2
+
+vim.opt_local.cursorcolumn = true
+
+local group = vim.api.nvim_create_augroup("YamlCursorColumn", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    buffer = 0,
+    callback = function()
+        vim.opt_local.cursorcolumn = true
+        vim.g.tscontext = true
+        require("treesitter-context").enable()
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+    group = group,
+    buffer = 0,
+    callback = function()
+        vim.opt_local.cursorcolumn = false
+        vim.g.tscontext = false
+        require("treesitter-context").disable()
+    end,
+})
