15748dfe87ea6c286e73733d14aaaa4c4e6a795b
Eloi Vallverdú <eleloi@gmail.com>
2025-10-13 10:34:53 +0200

    refactor(tailwind): Replace tailwind-tools plugin with tailwindcss-colorizer-cmp
    This change swaps out `luckasRanarison/tailwind-tools.nvim` for
    `roobert/tailwindcss-colorizer-cmp.nvim` to provide Tailwind CSS
    colorization, including configuration for color square width.

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,32 +1,30 @@
 -- View colors in certain files
 return {
-  {
-    "norcalli/nvim-colorizer.lua",
-    event = "VeryLazy",
-    config = function()
-      require("colorizer").setup({
-        "css",
-        "javascript",
-        "typesciprt",
-        "javascriptreact",
-        "typescriptreact",
-        "html",
-        "lua",
-        "python",
-        "json",
-        "conf",
-      })
-    end,
-  },
-  {
-    "luckasRanarison/tailwind-tools.nvim",
-    name = "tailwind-tools",
-    build = ":UpdateRemotePlugins",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim",
-      "neovim/nvim-lspconfig",
+    {
+        "norcalli/nvim-colorizer.lua",
+        event = "VeryLazy",
+        config = function()
+            require("colorizer").setup({
+                "css",
+                "javascript",
+                "typesciprt",
+                "javascriptreact",
+                "typescriptreact",
+                "html",
+                "lua",
+                "python",
+                "json",
+                "conf",
+            })
+        end,
     },
-    opts = {},
-  },
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        -- optionally, override the default options:
+        config = function()
+            require("tailwindcss-colorizer-cmp").setup({
+                color_square_width = 2,
+            })
+        end,
+    },
 }
