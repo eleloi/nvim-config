380b562b14fda453cf9d21f7731c11dcc866b314
eleloi <evallverdu@mossos.cat>
2026-03-09 06:01:49 +0100

    git: add hunk as jj split tool

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,40 +12,48 @@
 -- "rbong/vim-flog": Git branch viewer like git graph
 
 return {
-	{
-		"sindrets/diffview.nvim",
-		keys = {
-			{ "<leader>gd", ":DiffviewOpen<cr>", desc = "Diffview" },
-		},
-	},
-	{
-		"tpope/vim-fugitive",
-		cmd = { "G", "Git" },
-		event = "VeryLazy",
-		keys = {
-			{ "<leader>gg", ":tab Git<CR>", desc = "Git" },
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("BufReadPost", {
-				callback = function()
-					vim.g.bufhidden = "delete"
-				end,
-			})
-		end,
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signcolumn = false,
-			numhl = true,
-			max_file_length = 10000,
-		},
-	},
-	{
-		"junegunn/gv.vim",
-		dependencies = {
-			"tpope/vim-fugitive",
-		},
-		cmd = { "GV" },
-	},
+    {
+        "sindrets/diffview.nvim",
+        keys = {
+            { "<leader>gd", ":DiffviewOpen<cr>", desc = "Diffview" },
+        },
+    },
+    {
+        "julienvincent/hunk.nvim",
+        cmd = { "DiffEditor" },
+        dependencies = { "MunifTanjim/nui.nvim" },
+        config = function()
+            require("hunk").setup()
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = { "G", "Git" },
+        event = "VeryLazy",
+        keys = {
+            { "<leader>gg", ":tab Git<CR>", desc = "Git" },
+        },
+        config = function()
+            vim.api.nvim_create_autocmd("BufReadPost", {
+                callback = function()
+                    vim.g.bufhidden = "delete"
+                end,
+            })
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signcolumn = false,
+            numhl = true,
+            max_file_length = 10000,
+        },
+    },
+    {
+        "junegunn/gv.vim",
+        dependencies = {
+            "tpope/vim-fugitive",
+        },
+        cmd = { "GV" },
+    },
 }
