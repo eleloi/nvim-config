b1ab5eaf14a54fbbb95b571c8fc650cdaf51d0ca
eleloi <evallverdu@mossos.cat>
2026-04-02 02:26:22 +0200

    feat(which-key): upgrade to v3 with modern theme and icons

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,39 +1,33 @@
 return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		config = function()
-			local wk = require("which-key")
-			wk.setup({
-				delay = 1000,
-			})
-			wk.add({
-				{ "<leader>R", desc = "Reload configuration" },
-				{ "<leader>a", desc = "+ai" },
-				{ "<leader>b", desc = "+buffer" },
-				{ "<leader>c", group = "code" },
-				{ "<leader>cD", desc = "Type Definition" },
-				{ "<leader>ca", desc = "Code Action" },
-				{ "<leader>cr", desc = "Rename" },
-				{ "<leader>f", group = "find" },
-				{ "<leader>fc", desc = "Colorschemes" },
-				{ "<leader>g", desc = "+git" },
-				{ "<leader>h", group = "hardtime/http rest" },
-				{ "<leader>hc", desc = "🧠 Codeium" },
-				{ "<leader>hl", desc = "Run last http request" },
-				{ "<leader>hn", desc = "Run near http request" },
-				{ "<leader>s", desc = "+harpoon" },
-				{ "<leader>w", desc = "+save" },
-				{ "<leader>x", desc = "+funny" },
-				{ "g", group = "goto" },
-				{ "gD", desc = "Goto Definition vsplit" },
-				{ "gd", desc = "Goto Definition" },
-				{ "gi", desc = "Goto Implementation" },
-				{ "gr", desc = "Goto References" },
-				{ "gw", desc = "Wrap lines" },
-			})
-			wk.setup()
-		end,
-	},
-	{ "meznaric/key-analyzer.nvim", opts = {} },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            preset = "modern", -- Estilo limpio y moderno
+            delay = 500, -- Reducimos el retardo para que sea más ágil
+            win = {
+                border = "rounded",
+                padding = { 1, 2 },
+            },
+            spec = {
+                { "<leader>R", desc = "Reload configuration", icon = "󰑓 " },
+                { "<leader>a", group = "ai", icon = "󰧑 " },
+                { "<leader>b", group = "buffer", icon = "󰓩 " },
+                { "<leader>c", group = "code", icon = "󰅱 " },
+                { "<leader>f", group = "find", icon = "󰍉 " },
+                { "<leader>g", group = "git", icon = "󰊢 " },
+                { "<leader>h", group = "http/rest", icon = "󰖟 " },
+                { "<leader>s", group = "harpoon", icon = "󱗓 " },
+                { "<leader>u", group = "ui", icon = "󰙵 " }, -- Tu nuevo grupo de UI
+                { "<leader>w", group = "save", icon = "󰆓 " },
+                { "<leader>x", group = "funny", icon = "󰃥 " },
+                { "g", group = "goto", icon = "󰈇 " },
+            },
+        },
+        config = function(_, opts)
+            local wk = require("which-key")
+            wk.setup(opts)
+        end,
+    },
+    { "meznaric/key-analyzer.nvim", opts = {} },
 }
