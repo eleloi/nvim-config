3a664c858fac0f05be3d92a38db80e15e76d72fd
eleloi <evallverdu@mossos.cat>
2026-06-18 21:49:49 +0200

    feat(jj): add some plugins to manage splits and have highlight with jj

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,13 +1,18 @@
 vim.pack.add({
-    "https://github.com/rafikdraoui/jj-diffconflicts",
-    "https://github.com/lewis6991/gitsigns.nvim",
-    "https://github.com/wintermute-cell/gitignore.nvim",
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/wintermute-cell/gitignore.nvim",
+  "https://github.com/rafikdraoui/jj-diffconflicts",
+  "https://github.com/julienvincent/hunk.nvim",     -- to use in conjuntion with jj
+  "https://github.com/MunifTanjim/nui.nvim",        -- dependency of Hunk
+  "https://github.com/nvim-tree/nvim-web-devicons", -- dependency of Hunk
 })
 
 require("gitsigns").setup({
-    signcolumn = false, -- Don't use a separate sign column
-    numhl = true,    -- Highlight line number
-    linehl = false,
-    word_diff = false,
-    max_file_length = 10000,
+  signcolumn = false, -- Don't use a separate sign column
+  numhl = true,       -- Highlight line number
+  linehl = false,
+  word_diff = false,
+  max_file_length = 10000,
 })
+
+require("hunk").setup({})
