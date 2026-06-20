75c68cb60238637cd5fe425d113d679568d2e795
eleloi <evallverdu@mossos.cat>
2026-06-20 15:14:13 +0200

    remove: jj-log plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,7 @@
 vim.pack.add({
   "https://github.com/nvim-lualine/lualine.nvim",
-  "https://github.com/nvim-tree/nvim-web-devicons",       -- dependency
-  'https://tangled.org/bpavuk.neocities.org/jj-log.nvim', -- jj log access
+  "https://github.com/nvim-tree/nvim-web-devicons", -- dependency
 })
-
-require('jj-log').setup({
-  -- these are default options
-  out_waiting = "...",
-  out_no_jj_repo = "~no_jj~",
-  out_no_desc = "~no_desc~",
-  max_desc_length = 47, -- including truncation dots, that's 50
-})
-
 
 require("lualine").setup({
   options = {
@@ -23,9 +13,6 @@ require("lualine").setup({
   sections = {
     lualine_a = { { "mode", upper = true } },
     lualine_b = {
-      function()
-        return vim.b.jj_desc or ""
-      end,
       { "branch", icon = "" },
       "db_ui#statusline",
     },
