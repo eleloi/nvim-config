d6ac1dab72005f88b61775a9a43999dc5b11699d
eleloi <evallverdu@mossos.cat>
2026-06-22 13:24:49 +0200

    refactor(lsp): use lspconfig for golang and gopls

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,60 +1,44 @@
 vim.cmd("packadd nvim-lspconfig")
 vim.lsp.config("gopls", {
-    cmd = { "gopls" },
-    filetypes = { "go", "gomod", "gowork", "gotmpl" },
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-                shadow = true,
-            },
-            staticcheck = true,
-            gofumpt = true,
-        },
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
     },
-})
-
-vim.lsp.config("golangci_lint_ls", {
-    cmd = { "golangci-lint-langserver" },
-    filetypes = { "go", "gomod" },
-    init_options = {
-        command = {
-            "golangci-lint",
-            "run",
-            "--out-format",
-            "json",
-            "--issues-exit-code=1",
-        },
-    },
+  },
 })
 
 vim.lsp.enable("gopls")
 vim.lsp.enable("golangci_lint_ls")
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    desc = "Organize imports (same functionality as goimports but using lsp)",
-    callback = function(args)
-        -- 1. Check for gopls
-        local clients = vim.lsp.get_clients({ bufnr = args.buf, name = "gopls" })
-        if #clients == 0 then
-            return
+  pattern = "*.go",
+  desc = "Organize imports (same functionality as goimports but using lsp)",
+  callback = function(args)
+    -- 1. Check for gopls
+    local clients = vim.lsp.get_clients({ bufnr = args.buf, name = "gopls" })
+    if #clients == 0 then
+      return
+    end
+    local client = clients[1]
+
+    local params = vim.lsp.util.make_range_params(0, client.offset_encoding)
+    params.context = { only = { "source.organizeImports" } }
+
+    local result = vim.lsp.buf_request_sync(args.buf, "textDocument/codeAction", params, 1000)
+
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local c = vim.lsp.get_client_by_id(cid)
+          local enc = (c and c.offset_encoding) or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
         end
-        local client = clients[1]
-
-        local params = vim.lsp.util.make_range_params(0, client.offset_encoding)
-        params.context = { only = { "source.organizeImports" } }
-
-        local result = vim.lsp.buf_request_sync(args.buf, "textDocument/codeAction", params, 1000)
-
-        for cid, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-                if r.edit then
-                    local c = vim.lsp.get_client_by_id(cid)
-                    local enc = (c and c.offset_encoding) or "utf-16"
-                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
-                end
-            end
-        end
-    end,
+      end
+    end
+  end,
 })
